134d91d5b19e5b111c25f06c9e7ff68bf461196b
rachmat-transfashion <rachmat@transfashionindonesia.com>
2024-11-01 06:41:37 +0000

    testing saldo init

--- src/SaldoInit.php
+++ src/SaldoInit.php
@@ -23,14 +23,13 @@ class SaldoInit {
 			self::$syncSaldo = new SyncSaldo();
 			self::$syncItem = new SyncItem();
 
-
 			self::getActiveItem($batchid, $regions);							// ambil data master item aktiv dari transbrowser
-			//self::getItemStockPeriode($batchid, $regions, $periode);			// ambil data saldo stok pada suatu periode dari TransBrowser
-			//self::cekDataMapping($batchid, $regions, $periode);					// Cek Data dahulu sebelum lanjut proses ke main database
-			//self::PrepareTemporaryItemSaldo($batchid, $regions, $periode);		// siapkan data saldo per item (sizing) di temporary table
-			//self::SetupMerchItem($batchid, $regions, $periode);					// setup master itemstock, merchitem, mercharticle di main database
-			//self::CekPeriodeSaldoMerchItemIntegrity($periode);					// cek heinvitem di tmp_heinvitemsaldo apakah sudah ada semua di merchitem
-			//self::ApplyItemStockPeriode($batchid, $regions, $periode);			// apply saldo dari tmp_heinvitemsaldo ke itemstockmoving main database
+			self::getItemStockPeriode($batchid, $regions, $periode);			// ambil data saldo stok pada suatu periode dari TransBrowser
+			self::cekDataMapping($batchid, $regions, $periode);					// Cek Data dahulu sebelum lanjut proses ke main database
+			self::PrepareTemporaryItemSaldo($batchid, $regions, $periode);		// siapkan data saldo per item (sizing) di temporary table
+			self::SetupMerchItem($batchid, $regions, $periode);					// setup master itemstock, merchitem, mercharticle di main database
+			self::CekPeriodeSaldoMerchItemIntegrity($periode);					// cek heinvitem di tmp_heinvitemsaldo apakah sudah ada semua di merchitem
+			self::ApplyItemStockPeriode($batchid, $regions, $periode);			// apply saldo dari tmp_heinvitemsaldo ke itemstockmoving main database
 			
 			Log::info('DONE.');
 		} catch (\Exception $e) {
